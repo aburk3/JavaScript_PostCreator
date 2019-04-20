d70b8b2d91c0fddbfd0b1f89bd1edd657e83c679
aburk3 <aburk3@gmail.com>
2019-04-20 15:18:08 -0500

    Added to ApplicationController basic code to check if user has logged in and set_cache_headers

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -9,3 +9,19 @@ const bindClickHandlers = () => {
     history.pushState(null, null, "/posts")
     getPosts()
   })
+
+  $(document).on('click', ".show_link", function(e) {
+    e.preventDefault()
+    $('#app-container').html('')
+    let id = $(this).attr('data-id')
+    fetch(`/posts/${id}.json`)
+    .then(res => res.json())
+    .then(post => {
+      let newPost = new Post(post)
+
+      let postHtml = newPost.formatShow()
+
+      $('#app-container').append(postHtml)
+    })
+  })
+}
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :current_user
+  before_action :require_logged_in, except: [:new, :create]
+  before_action :set_cache_headers
+
+  def logged_in?
+    !!current_user
+  end
+
+
+  private
+    def require_logged_in
+      redirect_to root_path unless logged_in?
+    end
+
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def set_cache_headers
+      response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+      response.headers["Pragma"] = "no-cache"
+      response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+    end
+
+    helper_method :current_user
+
 end
