a6fcf6320df89bffb91ec2cdd982c5e1f8854133
aburk3 <aburk3@gmail.com>
2019-04-20 15:26:20 -0500

    Added users_controller and corresponding methods

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,56 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def new
+    @user = User.new
+  end
+
+  def index
+    @clubs = Club.all
+  end
+
+  def show
+    @clubs = Club.last(2)
+    @users = User.all
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "Update Successful."
+    else
+      render :edit, notice: "Failed to Update."
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(
+        :first_name,
+        :last_name,
+        :email,
+        :password,
+        :state,
+        :city
+      )
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+end
