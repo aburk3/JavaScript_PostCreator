c858d1da84b218698befc63ba72fbfe157f97984
aburk3 <aburk3@gmail.com>
2019-04-20 15:37:50 -0500

    Added routes for users, posts and nested comments, sessions, and sign-in/sing-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+
+  resources :posts do
+    get "/posts/:id/comments/new", to: 'comments#new'
+    resources :comments do
+      get "/delete", to: "comments#destroy"
+    end
+  end
+
+  root "sessions#new"
+  get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
+  get "/signout", to: "sessions#destroy"
 end
